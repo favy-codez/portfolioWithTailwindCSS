19b8c763b32717de6e43bb989ab46ef3ebeaaef5
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-07-30 22:07:55 +0100

    contact footer

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const hamburger = document.querySelector("#hamburger"); 
 const menu = document.querySelector("#menu");
 const hamtext = document.querySelector("#hamtext");
+const moon = document.querySelector("#moon");
+const body = document.querySelector("body")
+const hLinks = document.querySelectorAll("#hLink");
 
 hamburger.addEventListener("click", ()=>{
     menu.classList.toggle("hidden")
     // hamburger.classList.toggle("bg-white")
     hamtext.classList.toggle("bg-white")
 })
+
+hLinks.forEach(link=>{
+    link.addEventListener("click", ()=>{
+      menu.classList.toggle("hidden")
+      hamburger.classList.toggle("bg-white")
+    })
+  })
+  
+  moon.addEventListener("click", ()=>{
+    body.classList.toggle("dark")
+  })
